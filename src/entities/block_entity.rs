3ce812f81739d4bc3ea9bbbdb7b9615a558ff141
Winter <huangdongdong910@163.com>
2024-11-19 16:40:17 +0800

    ops: debug pg pool

--- src/entities/block_entity.rs
+++ src/entities/block_entity.rs
@@ -2,10 +2,7 @@ diesel::table! {
     use diesel::sql_types::*;
 
     #[sql_name="block"]
-    table_block(column_id) {
-        #[sql_name = "id"]
-        column_id -> Int8,
-
+    table_block(column_slot) {
         #[sql_name = "slot"]
         column_slot -> Int8,
     }
--- src/models/block_model.rs
+++ src/models/block_model.rs
@@ -8,10 +8,6 @@ use crate::entities::block_entity::table_block;
 #[diesel(table_name = table_block)]
 pub struct BlockRow {
     #[diesel(sql_type = Int8)]
-    #[diesel(column_name = column_id)]
-    pub id: i64,
-
-    #[diesel(sql_type = Int8)]
     #[diesel(column_name = column_slot)]
     pub slot: i64,
 }
